da0f972605c3f4a03c95f5140757ce3c27582e18
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-02-12 20:14:16 +0100

    testing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,31 @@
 console.log("test");
 
 
+
+/* 
+getElementById
+anzahl = 0
+anazahl += 1
+innerHTML
+*/
+
+
+const btn = document.querySelector("#clickMe");
+
+// counter 
+let counter = 0;
+
+
+// function 
+// wenn auf Button geclickt wird, wird hochgezählt
+btn.addEventListener("click", () => {
+    // counter ändern -> hochzählen
+counter = counter + 1;
+console.log(counter)
+
+// innerHTML schreiben
+document.querySelector("#clickMe").innerHTML = `Click me: ${counter}`;
+});
+
+
+
